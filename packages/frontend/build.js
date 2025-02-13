79ae5b87e5dbf44c99a864b8eba7ecff80d870d0
Cotholist <1810601166hank@gmail.com>
2025-02-13 10:57:36 +0800

    fix: simplify build process

--- packages/frontend/build.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-const path = require('path');
-
-// 确保 dist 目录存在并为空
-fs.emptyDirSync(path.join(__dirname, 'dist'));
-
-// 复制 src 目录到 dist
-fs.copySync(path.join(__dirname, 'src'), path.join(__dirname, 'dist'));
